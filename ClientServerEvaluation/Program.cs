e90bac3a662ee0b54c94f28c0fd5e43ac0a4360a
Dang Tuan Pham <dinhbinh599@gmail.com>
2023-04-03 00:36:07 +0700

    push

---
+++ ClientServerEvaluation/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClientServerEvaluation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientServerEvaluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SchoolDbContext db = new SchoolDbContext();
+            var davidName = db.Students.Where(x => x.FirstName == "David").FirstOrDefault();
+            Console.WriteLine(davidName?.FirstName);
+        }
+    }
+}
